9e8f07fef28decdf2cac5ee7594c77ec446f5561
Daru <mica.tui@proton.me>
2026-04-05 15:06:09 +0200

    xbyak: 7.24.2 -> 7.35.4
    Diff: https://github.com/herumi/xbyak/compare/v7.24.2...v7.35.4

--- packages/xbyak/xbyak.nix
+++ packages/xbyak/xbyak.nix
@@ -9,13 +9,13 @@
 
 stdenv.mkDerivation rec {
   pname = "xbyak";
-  version = "7.24.2";
+  version = "7.35.4";
 
   src = fetchFromGitHub {
     owner = "herumi";
     repo = "xbyak";
     rev = "v${version}";
-    hash = "sha256-WCwXVTGV/e+H6ymMmEPEiJE/saylptYQrcEEdfc4u/w=";
+    hash = "sha256-UPqqsG7GeWelOO9AKzveLNa1AYcHFBU6eLjOghDuS1s=";
   };
 
   nativeBuildInputs = [
@@ -28,7 +28,7 @@ stdenv.mkDerivation rec {
     description = "A JIT assembler for x86/x64 architectures supporting MMX, SSE (1-4), AVX (1-2, 512), FPU, APX, and AVX10.2";
     homepage = "https://github.com/herumi/xbyak";
     license = lib.licenses.bsd3;
-    maintainers = with lib.maintainers; [ daru-san];
+    maintainers = with lib.maintainers; [ daru-san ];
     mainProgram = "xbyak";
     platforms = lib.platforms.all;
   };
